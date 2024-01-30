53c25f249dfda23c656c52c0af3e69ab62d83982
rooneybewildin <rooneyrojas2004@gmail.com>
2024-01-29 16:20:29 -0800

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,28 @@
 # TODO Create your inputs, variables, and add your story here!
+#ask for a noun
+
+noun = input("Enter a noun: ")
+
+#ask for a verb
+
+verb = input("Enter a verb: ")
+
+#ask for an adjective
+
+adjective = input("Enter an adjective: ")
+
+#once all the inputs have been taken, print out a story using the inputted information. This story should make use of all the variables taken
+
+print(f"{noun} was {adjective} while {verb}")
+
+#add 10 more inputs and update your story to use those inputs
+
+#line 1
+
+noun = input("Enter a noun: ")
+
+verb = input("Enter a verb: ")
+
+adjective = input("Enter an adjective: ")
+
+print(f"{noun} was {adjective} while {verb}")# TODO Create your inputs, variables, and add your story here!
