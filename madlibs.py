ee377093f16696208b3456cdee50adc9bd6fd43e
rooneybewildin <rooneyrojas2004@gmail.com>
2024-01-29 20:07:50 -0800

    rooney's madlibs

--- madlibs.py
+++ madlibs.py
@@ -1,11 +1,11 @@
 # TODO Create your inputs, variables, and add your story here!
 #ask for a noun
 
-noun = input("Enter a noun: ")
+noun = input("Enter a name: ")
 
 #ask for a verb
 
-verb = input("Enter a verb: ")
+verb = input("Enter an 'ing' verb: ")
 
 #ask for an adjective
 
@@ -19,10 +19,63 @@ print(f"{noun} was {adjective} while {verb}")
 
 #line 1
 
-noun = input("Enter a noun: ")
+noun = input("Enter a noun for an object: ")
+verb = input("Enter an 'ing' verb: ")
+adjective = input("Enter an adjective to describe an action: ")
+print(f"They got a {noun} while they were {verb}. It was {adjective}")
 
+#line 2
+
+noun = input("Enter a plural noun for an animal: ")
+verb = input("Enter an 'ing' verb: ")
+adjective = input("Enter an adjective to describe animals: ")
+print(f"They met many {adjective} {noun} while they were {verb}")
+
+#line 3
+
+noun = input("Enter a place noun: ")
 verb = input("Enter a verb: ")
+adjective = input("Enter an adjective to describe animals: ")
+print(f"The animals showed them to a {adjective} {noun} to {verb}")
 
-adjective = input("Enter an adjective: ")
+#line 5
 
-print(f"{noun} was {adjective} while {verb}")# TODO Create your inputs, variables, and add your story here!
+noun = input("Enter a place noun: ")
+verb = input("Enter a verb: ")
+adjective = input("Enter an adverb for an verb: ")
+print(f"They {adjective} {verb} at a {verb}")
+
+#line 6
+
+noun = input("Enter a plural noun for an animal: ")
+verb = input("Enter a verb: ")
+adjective = input("Enter an adjective to describe animals: ")
+print(f"They met many {noun} while they were {verb}. They was {adjective}")
+
+#line 7
+
+noun = input("Enter a plural noun for an object: ")
+verb = input("Enter a verb: ")
+adjective = input("Enter an adjective to describe several things: ")
+print(f"Later, they {verb} many {adjective} {noun}.")
+
+#line 8
+
+noun = input("enter a name: ")
+verb = input("Enter a verb: ")
+adjective = input("Enter an adverb: ")
+print(f"While they did that, {noun} {verb} {adjective}.")
+
+#line 9
+
+noun = input("enter a building: ")
+verb = input("Enter a past tense verb: ")
+adjective = input("Enter an adverb: ")
+print(f"Suddenly, a {noun} was {verb} {adjective}.")
+
+#line 10
+
+noun = input("enter a name: ")
+verb = input("Enter a verb: ")
+adjective = input("Enter an adjective to describe a person's mood: ")
+print(f"{noun} wanted to check it out to {verb}. They were {adjective} about it.")
